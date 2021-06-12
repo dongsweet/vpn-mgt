e37ab65dc5e1347c6b373443f57f00deedfb5f08
Qing Dong <sweetreg@126.com>
2021-06-13 00:00:28 +0800

    Update config.default.js

--- config/config.default.js
+++ config/config.default.js
@@ -29,7 +29,8 @@ module.exports = appInfo => {
       app: true,
       // 是否加载到 agent 上，默认关闭
       agent: false,
-    }
+    },
+    proxy: true
   };
 
   // use for cookie sign key, should change to your own and keep security
